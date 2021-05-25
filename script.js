983466265c89a6a9d21a3a263dccd87fe0c3a451
Christine Ranum <chri89eq@easv365.dk>
2021-05-25 16:17:49 +0200

    Made corrections in order to validate CSS + HTML
    - Spelling errors corrected
    - Misplaced alt descriptions moved
    - Alt descriptions added
    - Unnecessary code removed
    - FIxed contact buttons and spacing

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var modal = document.getElementById("modal");
 // Get the button that opens the modal
 var btn = document.getElementById("modal_button");
 
-// Get the <span> element that closes the modal
+// Get the span element that closes the modal
 var span = document.getElementsByClassName("modal_close")[0];
 
 // When the user clicks on the button, open the modal 
@@ -15,7 +15,7 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on span (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
